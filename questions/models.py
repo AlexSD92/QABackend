d715cb08168df0d7b3959917ae757c9f0fbeaec5
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-16 19:45:13 +0100

    Question model created, migrations applied

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    question_summary = models.CharField(max_length=300, verbose_name='Question Summary')
+    question_details = models.TextField(verbose_name='Question Details')
+    question_author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Author')
+    question_published = models.DateTimeField(auto_now_add=True, verbose_name='Question Published')
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.question_summary
