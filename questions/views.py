1dc6101723e73e51d3eb24f6f950e90853d031ab
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-17 19:21:23 +0100

    Import Question from .models, QuestionsSerializer from .serializers and viewsets from rest_framework. QuestionViewSet created inheriting from viewsets.ModelViewSet

--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .models import Question
+from .serializers import QuestionsSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionsSerializer
+    lookup_field = 'slug'
+
