af3217eb733f30ebde5e724df6d0c2f2904b6233
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-18 20:28:16 +0100

    Set question_author to a StringRelatedField

--- questions/serializers.py
+++ questions/serializers.py
@@ -3,7 +3,7 @@ from .models import Question
 
 
 class QuestionsSerializer(serializers.ModelSerializer):
-    # question_author = serializers.StringRelatedField()
+    question_author = serializers.StringRelatedField(read_only=True)
     slug = serializers.SlugField(read_only=True)
     class Meta:
         model = Question
