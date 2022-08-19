98dd65f3a0d33f1caa152207351ef29ae1cbea21
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-19 19:47:23 +0100

    IsQuestionAuthor imported from .permissions, set permission_classes to IsQuestionAuthor for QuestionViewSet

--- questions/views.py
+++ questions/views.py
@@ -1,12 +1,15 @@
 from .models import Question
 from .serializers import QuestionsSerializer
 from rest_framework import viewsets
+from .permissions import IsQuestionAuthor
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionsSerializer
     lookup_field = 'slug'
+
+    permission_classes = [IsQuestionAuthor]
 
     def perform_create(self, serializer):
         serializer.save(question_author=self.request.user)
