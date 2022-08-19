5dd9ec0810c786be17d70fe78701fbe3d87fc2f8
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-19 19:48:12 +0100

    Edited to make slug generation more unique

--- questions/signals.py
+++ questions/signals.py
@@ -2,10 +2,11 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from .models import Question
 from django.utils.text import slugify
+from random import randrange
 
 
 @receiver(pre_save, sender=Question)
 def generate_slug(sender, instance, *args, **kwargs):
     if instance and not instance.slug:
-        slug = slugify(instance.question_summary)
+        slug = slugify(f"{instance.question_summary}-{randrange(10)}-{instance.question_author}")
         instance.slug = slug
