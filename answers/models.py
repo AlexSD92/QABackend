73bc56e306ba2f671e0d5a336647a8d291de5091
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-20 08:12:10 +0100

    Answer model created, registered in admin.py; Model contains a foreignkey field pointing to the Question model

--- answers/models.py
+++ answers/models.py
@@ -1,3 +1,16 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+from questions.models import Question
 
-# Create your models here.
+
+class Answer(models.Model):
+    answer_summary = models.CharField(max_length=300, verbose_name='Answer Summary')
+    answer_details = models.TextField(verbose_name='Answer Details')
+    answer_author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Author')
+    answer_published = models.DateTimeField(auto_now_add=True, verbose_name='Answer Published')
+    related_question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.answer_summary
