4d7f064f288d3330bb51551ff586430e35ad0578
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-21 10:12:53 +0100

    Adjusted URLs to reduce url length

--- qaapi/urls.py
+++ qaapi/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('questions.urls')),
-    path('api-auth/register/', include('dj_rest_auth.registration.urls')),
+    # path('api-auth/register/', include('dj_rest_auth.registration.urls')),
+    path('users/', include('users.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
 
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
--- questions/urls.py
+++ questions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import QuestionViewSet
+from answers.views import CreateAnswer, ListAnswer, AnswerDetails
 
 router = DefaultRouter()
 router.register('questions', QuestionViewSet, basename='questions')
@@ -8,8 +9,11 @@ router.register('questions', QuestionViewSet, basename='questions')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('questions/<slug:slug>/createanswer/', CreateAnswer.as_view()),
+    path('questions/<slug:slug>/listanswer/', ListAnswer.as_view()),
+    path('questions/<slug:slug>/listanswer/<int:pk>', AnswerDetails.as_view()),
     
-    # path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     
 
 ]
