2b428b77c4851d90fa8c0b192916019d9a27005c
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-21 10:11:36 +0100

    CreateAnswer inheriting from CreateAPIView; overwrides perform_create and associates to related question. ListAnswer inherting from ListAPIView filters by related question. AnswerDetails inherting from RetrieveUpdateDestroyAPIView allows for update and delete answer functionality

--- answers/views.py
+++ answers/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from .models import Answer
+from questions.models import Question
+from .serializers import AnswersSerializer
+from questions.serializers import QuestionsSerializer
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+# from .permissions import IsAnswerAuthor
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+class CreateAnswer(CreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswersSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        # slug = self.kwargs.get('slug')
+        question = get_object_or_404(Question, slug=self.kwargs.get('slug'))
+        serializer.save(answer_author=user, related_question=question)
+
+
+class ListAnswer(ListAPIView):
+    serializer_class = AnswersSerializer
+
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        return Answer.objects.filter(related_question__slug=slug)
+
+class AnswerDetails(RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswersSerializer
