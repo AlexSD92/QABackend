90dc8496dc013fc88b1cde08b8c991e7ee44ab7d
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-22 20:48:59 +0100

    Static files collected

--- qaapi/settings.py
+++ qaapi/settings.py
@@ -140,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
