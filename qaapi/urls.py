87a5f0291a98cf1a5ccb91da731c184e1ac7cb87
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-22 19:43:31 +0100

    Commented jwt token authentication to test more with tokenauth

--- qaapi/urls.py
+++ qaapi/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
@@ -12,4 +13,6 @@ urlpatterns = [
 
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
