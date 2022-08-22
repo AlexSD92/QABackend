5b4803b82de74b27fe15393dfc58d2cd9619c5ca
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-22 20:20:16 +0100

    Added URL path

--- qaapi/urls.py
+++ qaapi/urls.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('questions.urls')),
+    path('questions/', include('questions.urls')),
     # path('api-auth/register/', include('dj_rest_auth.registration.urls')),
     path('users/', include('users.urls')),
     path('api-token-auth/', views.obtain_auth_token),
