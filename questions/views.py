5d644837a79802734182d49aa5efbcc947894356
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-22 19:46:41 +0100

    Imported IsAuthenticated, set permission classes for QuestionViewSet to IsAuthenticated and IsQuestionAuthor

--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,7 @@
 from .models import Question
 from .serializers import QuestionsSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .permissions import IsQuestionAuthor
 
 
@@ -8,8 +9,8 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionsSerializer
     lookup_field = 'slug'
+    permission_classes = [IsAuthenticated, IsQuestionAuthor]
 
-    permission_classes = [IsQuestionAuthor]
 
     def perform_create(self, serializer):
         serializer.save(question_author=self.request.user)
