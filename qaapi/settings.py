ed380391098a8aa47f5e9fb91723e567eaadd751
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-28 09:21:53 +0100

    Added heroku to CORS_ALLOWED_ORIGINS

--- qaapi/settings.py
+++ qaapi/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://dashboard.heroku.com/",
 ]
 
 # Application definition
