1ee103833b635f6640720b280de8ea8cbe13cdb0
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-29 10:58:46 +0100

    Authentication changes

--- qaapi/settings.py
+++ qaapi/settings.py
@@ -157,12 +157,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,8 @@
 from .models import Question
 from .serializers import QuestionsSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+# from rest_framework.permissions import IsAuthenticated
 from .permissions import IsQuestionAuthor
 
 
@@ -9,7 +10,8 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionsSerializer
     lookup_field = 'slug'
-    permission_classes = [IsAuthenticated, IsQuestionAuthor]
+    permission_classes = [IsQuestionAuthor]
+    # authentication_classes = [TokenAuthentication, SessionAuthentication]
 
 
     def perform_create(self, serializer):
